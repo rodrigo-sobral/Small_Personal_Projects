8670bc636f46e782e39f955e742ca8004d9e4bd8
Rodrigo Sobral <rodrigosobral@sapo.pt>
2023-02-22 00:59:32 +0000

    frontend changes and utils unification

--- Python/Flyordie/main.py
+++ Python/Flyordie/main.py
@@ -2,11 +2,16 @@ import tkinter as tk
 from PIL import Image, ImageTk
 from requests import get
 from io import BytesIO
-from utils import add_player_input_field
-    
-class SampleApp(tk.Tk):
+from utils import add_player_input_field, NORMAL_FONT, BG_COLOR, TEXT_PROPS, TITLE_PROPS, GREEN_BUTTON_PROPS, LIGHTBLUE_BUTTON_PROPS, DARKRED_BUTTON_PROPS, export_players_data
+
+
+players_data = {}
+
+
+class Window(tk.Tk):
     ICON_URL = "https://www.flyordie.com/favicon.ico"
-    SCREEN_WIDTH, SCREEN_HEIGHT = 480, 320
+    MIN_SCREEN_WIDTH, MIN_SCREEN_HEIGHT = 380, 300
+    SCREEN_WIDTH, SCREEN_HEIGHT = 480, 580
 
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
@@ -28,6 +33,7 @@ class SampleApp(tk.Tk):
         self.title("FlyorDie - Fernando Sobral")
         self.geometry(f"{self.SCREEN_WIDTH}x{self.SCREEN_HEIGHT}")
         self.iconphoto(True, ImageTk.PhotoImage(Image.open(BytesIO(favicon))))
+        self.minsize(self.MIN_SCREEN_WIDTH, self.MIN_SCREEN_HEIGHT)
 
     def show_frame(self, page_name: str) -> None:
         frame = self.frames[page_name]
@@ -35,63 +41,61 @@ class SampleApp(tk.Tk):
 
 
 class MainMenu(tk.Frame):
-    NORMAL_FONT = ("Arial", 12)
-
     def __init__(self, parent: tk.Frame, controller: tk.Tk):
-        tk.Frame.__init__(self, parent)
+        tk.Frame.__init__(self, parent, bg=BG_COLOR)
         self.controller = controller
-        label = tk.Label(self, text="Menu Principal", font=("Arial", 24))
-        label.pack(side=tk.TOP, fill=tk.X, pady=10)
+        label = tk.Label(self, text="Menu Principal", **TITLE_PROPS)
+        label.pack(side=tk.TOP, pady=10)
 
-        button_players = tk.Button(self, text="Dados Jogadores", font=self.NORMAL_FONT, command=lambda: controller.show_frame("PlayersData"))
-        button_players.pack(pady=10)
+        button_players = tk.Button(self, text="Dados Jogadores", font=NORMAL_FONT, command=lambda: controller.show_frame("PlayersData"))
+        button_players.pack(side=tk.TOP, pady=10)
         
-        button_results = tk.Button(self, text="Simulador de Resultados", font=self.NORMAL_FONT, command=lambda: controller.show_frame("ResultSimulator"))
-        button_results.pack(pady=10)
+        button_results = tk.Button(self, text="Simulador de Resultados", font=NORMAL_FONT, command=lambda: controller.show_frame("ResultSimulator"))
+        button_results.pack(side=tk.TOP, pady=10)
         
-        button_exit = tk.Button(self, text="Sair", bg="darkred", fg="white", font=self.NORMAL_FONT, command=self.quit)
+        button_exit = tk.Button(self, text="Sair", **DARKRED_BUTTON_PROPS, command=self.quit)
         button_exit.pack(side=tk.BOTTOM, pady=10)
 
 
 class PlayersData(tk.Frame):
-    NORMAL_FONT = ("Arial", 12)
+    LIMIT_PLAYERS = 4
 
     def __init__(self, parent: tk.Frame, controller: tk.Tk):
-        tk.Frame.__init__(self, parent)
+        tk.Frame.__init__(self, parent, bg=BG_COLOR)
         self.controller = controller
-        
-        label = tk.Label(self, text="Dados dos Jogadores", font=("Arial", 24), pady=10)
-        label.pack(side=tk.TOP, fill=tk.X, pady=10)
+        label = tk.Label(self, text="Dados dos Jogadores", **TITLE_PROPS)
+        label.pack(side=tk.TOP, pady=10)
         
         add_player_input_field(self)
+
+        button_back = tk.Button(self, text="Voltar", font=NORMAL_FONT, command=lambda: controller.show_frame("MainMenu"))
+        button_back.pack(side=tk.BOTTOM, pady=10)
         
-        button_plus = tk.Button(self, text="+", font=self.NORMAL_FONT, command=lambda: add_player_input_field(self))
+        export_button = tk.Button(self, text="Exportar", **LIGHTBLUE_BUTTON_PROPS, command=lambda: export_players_data(players_data))
+        export_button.pack(side=tk.BOTTOM, padx=10, pady=5)
+        
+        button_plus = tk.Button(self, text="+", **GREEN_BUTTON_PROPS, command=lambda: add_player_input_field(self, can_delete=True))
         button_plus.pack(side=tk.BOTTOM, pady=10)
         
-        button_back = tk.Button(self, text="Voltar", font=self.NORMAL_FONT, command=lambda: controller.show_frame("MainMenu"))
-        button_back.pack(side=tk.BOTTOM, pady=10)
-
 
 
 class ResultSimulator(tk.Frame):
-    NORMAL_FONT = ("Arial", 12)
-
     def __init__(self, parent: tk.Frame, controller: tk.Tk):
-        tk.Frame.__init__(self, parent)
+        tk.Frame.__init__(self, parent, bg=BG_COLOR)
         self.controller = controller
-        label = tk.Label(self, text="Simulador de Resultados", font=("Arial", 24), pady=10)
+        label = tk.Label(self, text="Simulador de Resultados", **TITLE_PROPS)
         label.pack(side=tk.TOP, padx=10, pady=5)
         
-        input_player1 = add_player_input_field(self)
-        vs = tk.Label(self, text="VS")
-        vs.pack(pady=2)
-        input_player2 = add_player_input_field(self)
+        input_player1 = add_player_input_field(self, "Nome do Jogador")
+        vs = tk.Label(self, text="VS", **TEXT_PROPS)
+        vs.pack()
+        input_player2 = add_player_input_field(self, "Nome do Oponente")
         
-        generate_button = tk.Button(self, text="Gerar Resultado", bg="green", fg="white", font=self.NORMAL_FONT, command=lambda: self.generate_result())
+        generate_button = tk.Button(self, text="Gerar Resultado", **GREEN_BUTTON_PROPS, command=lambda: self.generate_result())
         generate_button.pack(side=tk.TOP, padx=10, pady=5)
         
-        button_back = tk.Button(self, text="Voltar", font=self.NORMAL_FONT, command=lambda: controller.show_frame("MainMenu"))
-        button_back.pack(side=tk.BOTTOM, padx=10, pady=5)
+        button_back = tk.Button(self, text="Voltar", font=NORMAL_FONT, command=lambda: controller.show_frame("MainMenu"))
+        button_back.pack(side=tk.BOTTOM, pady=10)
 
 
     def generate_result(self) -> None:
@@ -99,5 +103,5 @@ class ResultSimulator(tk.Frame):
 
 
 if __name__ == "__main__":
-    app = SampleApp()
+    app = Window()
     app.mainloop()
